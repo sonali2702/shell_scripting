d6e39a0ccb2becc69cbab0ec22bad9d8d92d6e73
sonali270702 <104364617+sonali270702@users.noreply.github.com>
2025-05-30 13:03:26 +0530

    add this file

---
+++ even-odd.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+echo -p " enter the number :"  num
+
+
+if ((num%2 ==0)); then
+    echo "$num is even "
+else
+     echo "$num is odd"
+fi
---
+++ file_exist.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+read -p "enter the filename: " file
+if [ -f "$file" ];then
+   echo "file exists."
+else
+   echo "file does not exist"
+fi 
